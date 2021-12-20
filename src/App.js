2d6b02cb0c6fb8a11ce8d25143f045222a6214d4
SenatorP51 <spg@spg51.net>
2021-12-20 17:18:27 +0100

    Nie wiem

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 111 <code>src/App.js</code> and save to reload.
+          Edit 666 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
